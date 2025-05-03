71d563166bf67f5e7c75cc7fab5cbd068b1b054a
La-Ronda <lalacha@MacBookPro.attlocal.net>
2025-05-03 13:56:48 -0500

    This version works and server connects

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(helmet());
 app.use (express.json());
-app.use( urlencoded());
-app.use(static());
+app.use(express.urlencoded({extended:true}));
+
 
 //routes with send method
 
